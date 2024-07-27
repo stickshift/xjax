f6466497143b3e4d854febb4a8ebf9ce6ad7a563
Andrew Young <joven@alum.wpi.edu>
2024-07-27 14:00:26 -0400

    lint

--- src/xjax/datasets.py
+++ src/xjax/datasets.py
@@ -31,3 +31,26 @@ def diagonal(*, rng: jax.Array, n: int | None = None) -> tuple[jax.Array, jax.Ar
     y = jnp.where(x1 >= x0, 1.0, 0.0)
 
     return X, y
+
+
+def circle(*, rng: jax.Array, n: int | None = None) -> tuple[jax.Array, jax.Array]:
+    """Generate circle dataset where y is 1 if point (x0, x1) is within 0.4 of center."""
+    # Defaults
+    n = default_arg(n, 10000)
+
+    # RNGs
+    x0_rng, x1_rng = jax.random.split(rng, 2)
+
+    # x0
+    x0 = jax.random.uniform(x0_rng, shape=(n,))
+
+    # x1
+    x1 = jax.random.uniform(x1_rng, shape=(n,))
+
+    # Stack x0, x1 into X
+    X = jnp.stack([x0, x1], axis=1)
+
+    # Generate y: 1 if point (x0, x1) is above diagonal
+    y = jnp.where(x1 >= x0, 1.0, 0.0)
+
+    return X, y
--- tests/unit/test_datasets.py
+++ tests/unit/test_datasets.py
@@ -1,4 +1,5 @@
 import jax
+from jax import numpy as jnp
 
 import xjax
 
@@ -22,9 +23,38 @@ def test_diagonal_dataset():
     # Thens
     #
 
-    # y should be 1 if x1 >= x0
+    # y should be 1 if point (x0, x1) is above diagonal
     for i in range(len(X)):
         if X[i][1] >= X[i][0]:
+            assert y[i] == 1.0
+        else:
+            assert y[i] == 0.0
+
+
+def test_circle_dataset():
+    #
+    # Givens
+    #
+
+    # I initialized rng
+    rng = jax.random.key(42)
+
+    #
+    # Whens
+    #
+
+    # I load circle dataset
+    X, y = xjax.datasets.circle(rng=rng)
+
+    #
+    # Thens
+    #
+
+    # y should be 1 if point (x0, x1) is within 0.4 from center (0.5, 0.5)
+    for i in range(len(X)):
+        x0, x1 = X[i][0], X[i][1]
+
+        if jnp.sqrt((x0 - 0.5) ** 2 + (x1 - 0.5) ** 2) <= 0.4:
             assert y[i] == 1.0
         else:
             assert y[i] == 0.0
